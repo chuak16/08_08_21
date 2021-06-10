076ee4f1b70785a55c426f60d1610ef9f22f90bc
Kenny Chua <chuak16@gmail.com>
2021-06-10 16:43:04 +0800

    working with embedded matplot into GUI

--- test.py
+++ test.py
@@ -2,8 +2,9 @@ import tkinter as tk
 from itertools import count
 from matplotlib.animation import FuncAnimation
 from scipy.integrate import odeint
-import keyboard
 from matplotlib import pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
 
 def tank(X, t, qin,A1,A2,R1,R2):
     h1, h2 = X
@@ -15,55 +16,67 @@ def tank(X, t, qin,A1,A2,R1,R2):
     return dhdt
 
 
-# def old_gui(A1,A2,R1,R2,qin):
-#     choice = int(
-#         input(
-#             f'1. Change A1\n2. Change A2\n3. Change R1\n4. Change R2\n5. Change Qin\n0. To plot graph\nYour choices:'))
-#     while choice != 0:
-#         if choice == 1:
-#             A1 = float(input("Input A1 value:"))
-#             print(f'\nA1 value:{A1}\nA2 value:{A2}\nR1 value:{R1}\nR2 value:{R2}\nQin value:{qin}\n')
-#         elif choice == 2:
-#             A2 = float(input("Input A2 value:"))
-#             print(f'\nA1 value:{A1}\nA2 value:{A2}\nR1 value:{R1}\nR2 value:{R2}\nQin value:{qin}\n')
-#         elif choice == 3:
-#             R1 = float(input("Input R1 value:"))
-#             print(f'\nA1 value:{A1}\nA2 value:{A2}\nR1 value:{R1}\nR2 value:{R2}\nQin value:{qin}\n')
-#         elif choice == 4:
-#             R2 = float(input("Input R2 value:"))
-#             print(f'\nA1 value:{A1}\nA2 value:{A2}\nR1 value:{R1}\nR2 value:{R2}\nQin value:{qin}\n')
-#         elif choice == 5:
-#             qin = float(input("Input qin value:"))
-#             print(f'\nA1 value:{A1}\nA2 value:{A2}\nR1 value:{R1}\nR2 value:{R2}\nQin value:{qin}\n')
-#         elif choice == 0:
-#             print(f'\nA1 value:{A1}\nA2 value:{A2}\nR1 value:{R1}\nR2 value:{R2}\nQin value:{qin}\n')
-#             break
-#         else:
-#             print("Input number 0 - 5 only")
-#         choice = int(input(
-#             f'1. Change A1\n2. Change A2\n3. Change R1\n4. Change R2\n5. Change Qin\n0.To plot graph\nYour choices:'))
-#     return A1,A2,R1,R2,qin
-
-
 def gui(A1,A2,R1,R2,qin):
     parameter = tk.Tk()
-    parameter.minsize(width=400, height=400)
+    parameter.minsize(width=1000, height=500)
     parameter.title("Adjusting Parameter")
+    tk.Label(parameter, text="Current Value").place(relx=0.15, rely=0.05, anchor='nw')
+    tk.Label(parameter, text="New Value").place(relx=0.25, rely=0.05, anchor='nw')
     tk.Label(parameter, text = "A1").place(relx = 0.1, rely = 0.1, anchor = 'nw')
+    tk.Label(parameter, text="A2").place(relx=0.1, rely=0.2, anchor='nw')
+    tk.Label(parameter, text="R1").place(relx=0.1, rely=0.3, anchor='nw')
+    tk.Label(parameter, text="R2").place(relx=0.1, rely=0.4, anchor='nw')
+    tk.Label(parameter, text="qin").place(relx=0.1, rely=0.5, anchor='nw')
     A1_entry = tk.Entry(parameter, width = 10)
-    A1_entry.place(relx = 0.4, rely = 0.1, anchor = 'nw')
+    A1_entry.place(relx = 0.25, rely = 0.1, anchor = 'nw')
+    A2_entry = tk.Entry(parameter, width=10)
+    A2_entry.place(relx=0.25, rely=0.2, anchor='nw')
+    R1_entry = tk.Entry(parameter, width=10)
+    R1_entry.place(relx=0.25, rely=0.3, anchor='nw')
+    R2_entry = tk.Entry(parameter, width=10)
+    R2_entry.place(relx=0.25, rely=0.4, anchor='nw')
+    qin_entry = tk.Entry(parameter, width=10)
+    qin_entry.place(relx=0.25, rely=0.5, anchor='nw')
     A1_variable = tk.StringVar()
     A1_variable.set(A1)
-    tk.Label(parameter, textvariable = A1_variable).place(relx = 0.2, rely = 0.1, anchor = 'nw')
-    tk.Button(parameter, text="Save", command=save_parameter).place(relx=0.5, rely=0.6, anchor="s")
-    return parameter, A1_entry, A1_variable
+    tk.Label(parameter, textvariable = A1_variable).place(relx = 0.15, rely = 0.1, anchor = 'nw')
+    A2_variable = tk.StringVar()
+    A2_variable.set(A2)
+    tk.Label(parameter, textvariable=A2_variable).place(relx=0.15, rely=0.2, anchor='nw')
+    R1_variable = tk.StringVar()
+    R1_variable.set(R1)
+    tk.Label(parameter, textvariable=R1_variable).place(relx=0.15, rely=0.3, anchor='nw')
+    R2_variable = tk.StringVar()
+    R2_variable.set(R2)
+    tk.Label(parameter, textvariable=R2_variable).place(relx=0.15, rely=0.4, anchor='nw')
+    qin_variable = tk.StringVar()
+    qin_variable.set(qin)
+    tk.Label(parameter, textvariable=qin_variable).place(relx=0.15, rely=0.5, anchor='nw')
+    tk.Button(parameter, text="Save", command=save_parameter).place(relx=0.22, rely=0.6, anchor="s")
+    return parameter, A1_entry, A1_variable, A2_entry, A2_variable, R1_entry, R1_variable, R2_entry, R2_variable, qin_entry, qin_variable
 
 
 def save_parameter():
     global A1
+    global A2
+    global R1
+    global R2
+    global qin
     if A1_entry.get():
-        A1 = A1_entry.get()
+        A1 = float(A1_entry.get())
+    if A2_entry.get():
+        A2 = float(A2_entry.get())
+    if R1_entry.get():
+        R1 = float(R1_entry.get())
+    if R2_entry.get():
+        R2 = float(R2_entry.get())
+    if qin_entry.get():
+        qin = float(qin_entry.get())
     A1_variable.set(A1)
+    A2_variable.set(A2)
+    R1_variable.set(R1)
+    R2_variable.set(R2)
+    qin_variable.set(qin)
 
 
 def live_data(i):
@@ -81,12 +94,10 @@ def live_data(i):
     plt.cla()
     plt.plot(x_vals, H1, 'b-', label='Tank 1')
     plt.plot(x_vals, H2, 'r-', label='Tank 2')
-    plt.axis([0, len(t), 0, 100])
     plt.legend(loc='upper right')
     plt.xlabel('Time(seconds)')
     plt.ylabel('Height [m]')
     plt.title('Simulation of Two Tanks in Series')
-    plt.tight_layout()
 
 
 A1 = 66.4424
@@ -100,10 +111,8 @@ H2 = [0]
 t = [0]
 index = count()
 
-# parameter, A1_entry, A1_variable= gui(A1,A2,R1,R2,qin)
-parameter, A1_entry, A1_variable = gui(A1, A2, R1, R2, qin)
-while True:
-    print("hi2")
-    animation = FuncAnimation(plt.gcf(), live_data, interval=1000)
-    print("hi3")
-    plt.show()
+parameter, A1_entry, A1_variable, A2_entry, A2_variable, R1_entry, R1_variable, R2_entry, R2_variable, qin_entry, qin_variable= gui(A1,A2,R1,R2,qin)
+canvas = FigureCanvasTkAgg(plt.gcf(), master=parameter)
+canvas.get_tk_widget().place(relx=0.35, rely=0.0, anchor='nw')
+animation = FuncAnimation(plt.gcf(), live_data, interval=10)
+parameter.mainloop()
